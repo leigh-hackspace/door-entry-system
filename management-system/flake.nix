c3210ae1accb8a48c47d65593f2f5d0f7966b785
Chris Dell <cjdell@gmail.com>
2025-01-21 18:41:51 +0000

    Update hash

--- management-system/flake.nix
+++ management-system/flake.nix
@@ -24,7 +24,7 @@
         let
           hashes = {
             aarch64-darwin = "sha256-N4GxH/ItKUSatEq7NiMqgzvIS5bIZ8u9itKoVdhTz6g=";
-            x86_64-linux = "sha256-kP1SZxco4/U2h4yR1QEM5+BYkt3MAcWqgZbeavARiDQ=";
+            x86_64-linux = "sha256-74eD29QkBUfI4bUvz9mmlUjnuy4JwpV3ZH8AK/MUvcU=";
           };
         in
         pkgs.stdenv.mkDerivation {
