911f79342f86ed4095dc7147c8dee30c93b0a357
Chris Dell <cjdell@gmail.com>
2025-02-19 17:20:28 +0000

    Update hash

--- management-system/flake.nix
+++ management-system/flake.nix
@@ -14,7 +14,7 @@
         packages.default = let
           hashes = {
             aarch64-darwin = "sha256-N4GxH/ItKUSatEq7NiMqgzvIS5bIZ8u9itKoVdhTz6g=";
-            x86_64-linux = "sha256-hfoqT0nQMMggpcgqTjupMud8gnPBjuqUqryf8Ei/UK4=";
+            x86_64-linux = "sha256-n2toaousMTQGXtEOosf7/LTH8rpm234kJvpRYhblmKs=";
           };
         in pkgs.stdenv.mkDerivation {
           pname = "door-entry-management-system";
